0a42a5b5a82dc71ce4ab466d90be7e8fbdb11799
nico <ngdiaz00@gmail.com>
2025-01-22 10:08:55 -0300

    Se agrega Propiedad Pass

--- dominio/Persona.cs
+++ dominio/Persona.cs
@@ -9,7 +9,7 @@ namespace dominio
 {
     public  abstract class Persona
     {
-        
+        public int Id {  get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Dni { get; set; }
