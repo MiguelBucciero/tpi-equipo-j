35659179a19b1289ca50ebee30761dd9cb5bad62
nico <ngdiaz00@gmail.com>
2025-01-22 10:08:21 -0300

    Se crea listar alumno

--- negocio/AlumnoNegocio.cs
+++ negocio/AlumnoNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,50 @@ using System.Threading.Tasks;
 
 namespace negocio
 {
-    internal class AlumnoNegocio
+    public class AlumnoNegocio
     {
+       
+        public List<Alumno> listarAlumnos()
+        {
+            string consulta = "select Id, Nombre, Dni, FechaNacimiento, Sexo, Domicilio, Altura, Peso, Pass, Email from ALUMNOS";
+            List<Alumno> listaAlumnos = new List<Alumno>();
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+
+            try
+            {
+                accesoDatos.setearConsulta(consulta);
+                accesoDatos.ejecutarLectura();
+
+                while (accesoDatos.Lector.Read())
+                {
+                    Alumno alumno = new Alumno();
+                    alumno.Id = (int)accesoDatos.Lector["Id"];
+                    alumno.Nombre = (string)accesoDatos.Lector["Nombre"];
+                    alumno.Dni = (string)accesoDatos.Lector["Dni"];
+                    alumno.FechaNacimiento = (DateTime)accesoDatos.Lector["FechaNacimiento"];
+                    alumno.Sexo = (string)accesoDatos.Lector["Sexo"];
+                    alumno.Domicilio = (string)accesoDatos.Lector["Domicilio"];
+                    alumno.Altura = (string)accesoDatos.Lector["Altura"];
+                    alumno.Peso = (decimal)accesoDatos.Lector["Peso"];
+                    alumno.Pass = (string)accesoDatos.Lector["Pass"];
+                    alumno.Email = (string)accesoDatos.Lector["Email"];
+
+                    listaAlumnos.Add(alumno);
+
+
+                }
+                return listaAlumnos;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
     }
 }
